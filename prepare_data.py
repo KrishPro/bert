849c24f971144389e7017df01c3eea8123a97193
Krish Baisoya <krishbaisoya.it2020@gmail.com>
2022-08-23 01:58:39 +0530

    Created a function to split files

--- prepare_data.py
+++ prepare_data.py
@@ -6,6 +6,7 @@ filename: `prepare_data.py`
 
 import multiprocessing
 import os
+from typing import List, Tuple
 from tqdm import tqdm
 
 import string
@@ -13,7 +14,7 @@ import emoji
 import nltk
 import re
 
-from utils import embed_num_sentences_into_file, ensure_parent_dir, extract_num_sentences
+from utils import embed_num_sentences_into_file, ensure_dir, ensure_parent_dir, extract_num_sentences
 
 
 class ExtractEnglish:
@@ -56,3 +57,21 @@ def prepare_file(input_path: str, output_path: str, chunk_size = 512):
             output_file.write('\n'.join(chunk) + '\n')
 
     embed_num_sentences_into_file(output_path)
+
+
+def split_data(input_path: str, output_dir: str, splits: List[Tuple[str, float]]):
+    num_sentences = extract_num_sentences(input_path)
+    ensure_dir(output_dir)
+
+    with open(input_path) as input_file:
+        for name, size in splits:
+            with open(os.path.join(output_dir, name+'.txt'), 'w') as split_file:
+                for _ in tqdm(range(int(size * num_sentences)), desc=f"Creating {name} split"):
+                    try:
+                        split_file.write(next(input_file))
+                    except StopIteration:
+                        break
+                    
+
+            embed_num_sentences_into_file(os.path.join(output_dir, name+'.txt'))
+            print()
